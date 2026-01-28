79a35feb13cab0f8e58f57a5a28757c03c84d716
Jones. <jonesmailhub@gmail.com>
2026-01-28 23:26:24 +0500

    Create 0367.py

---
+++ 0367.py
@@ -0,0 +1,5 @@
+names = ["Катя", "Алина", "Яна"]
+
+if "Катя" in names: print("Привет, " + names[0] + "!")
+if "Алина" in names: print("Привет, " + names[1] + "!")
+if "Яна" in names: print("Привет, " + names[2] + "!")
