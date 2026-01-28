1d295bed68033487a0cf56917166a27018e6941c
Jones. <jonesmailhub@gmail.com>
2026-01-28 20:51:37 +0500

    Create 0241.py

---
+++ 0241.py
@@ -0,0 +1,10 @@
+word1 = "Dron"
+print(word1.lower())
+
+word2 = "EARTH"
+print(word2.lower())
+
+word3 = "mOtOrBiKe"
+print(word3.lower())
+
+# Метод lower возвращает строку, в которой все символы строки находятся в нижнем регистре.
