ffe8ee3569457808d857ea8ce5496fe77ee908e2
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:10:47 +0500

    Create 0286.py

---
+++ 0286.py
@@ -0,0 +1,6 @@
+tuple = (5, 3, 8, 3, 7, 5, 3, 7)
+
+print("Число " + str(tuple[1]) + ": " + str(tuple.count(3)))
+print("Число " + str(tuple[0]) + ": " + str(tuple.count(5)))
+print("Число " + str(tuple[4]) + ": " + str(tuple.count(7)))
+print("Число " + str(tuple[2]) + ": " + str(tuple.count(8)))
