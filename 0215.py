0f983707af282c9e9d0f47a848b62eacf73e226c
Jones. <jonesmailhub@gmail.com>
2026-01-28 20:16:39 +0500

    Create 0215.py

---
+++ 0215.py
@@ -0,0 +1,7 @@
+a = "basket"
+b = "ball"
+c = "volley"
+
+print("I don't like " + a + b + " and " + c + b + " too.")
+
+# Конкатенация строк в Python — это операция соединения двух или более строк в одну. 
