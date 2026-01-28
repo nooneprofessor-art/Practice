b629f4d08a21abda100f7766e8fd54aa229a6b2c
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:39:23 +0500

    Create 0342.py

---
+++ 0342.py
@@ -0,0 +1,11 @@
+list1 = [i + 5 for i in range(10)]
+print(list1)
+
+list2 = [i * 5 for i in range(10)]
+print(list2)
+
+list3 = [i ** 3 for i in range(10)]
+print(list3)
+
+list4 = [i % 2 for i in range(10)]
+print(list4)
