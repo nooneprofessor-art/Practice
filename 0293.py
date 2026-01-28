eb498c374170b9832e5bd984ca7a971869c45f06
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:15:30 +0500

    Create 0293.py

---
+++ 0293.py
@@ -0,0 +1,15 @@
+twd_names = {
+"Rick",
+"Karl",
+"Daryl",
+"Mishoon",
+"Nigan",
+"Shein"
+}
+
+twd_names.remove("Mishoon")
+twd_names.remove("Nigan")
+twd_names.add("Bob")
+twd_names.add("T-Dog")
+
+print(twd_names)
