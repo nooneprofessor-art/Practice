49b65aa5b35769fe5c4ad6b6da26a87924fefaf2
Jones. <jonesmailhub@gmail.com>
2026-01-28 23:31:40 +0500

    Create 0376.py

---
+++ 0376.py
@@ -0,0 +1,8 @@
+import turtle
+joe = turtle.Turtle()
+
+for i in range(4):
+    joe.forward(100)
+       joe.left(90)
+
+# turtle — это модуль в языке программирования Python, который предоставляет простой способ визуализации кода через черепашью графику.
