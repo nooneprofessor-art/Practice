e942831e4a9122a6cdcceadea4ca231107dc5a19
Jones. <jonesmailhub@gmail.com>
2026-01-28 23:22:37 +0500

    Create 0359.py

---
+++ 0359.py
@@ -0,0 +1,24 @@
+el = ("⬛", "⬜", "🟫")
+
+print("WELCOME TO MY TIC TAC TOE GAME! 🕹️\n")
+
+print(el[0] * 13)
+
+print(el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0])
+print(el[0] + el[1] + "1 " + el[1] + el[0] + el[1] + "2 " + el[1] + el[0] + el[1] + "3 " + el[1] + el[0])
+print(el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0])
+
+print(el[0] * 13)
+
+print(el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0])
+print(el[0] + el[1] + "4 " + el[1] + el[0] + el[1] + "5 " + el[1] + el[0] + el[1] + "6 " + el[1] + el[0])
+print(el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0])
+    
+print(el[0] * 13)
+
+print(el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0])
+print(el[0] + el[1] + "7 " + el[1] + el[0] + el[1] + "8 " + el[1] + el[0] + el[1] + "9 " + el[1] + el[0])
+print(el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0] + el[1] * 3 + el[0])
+
+print(el[0] * 13)
+
