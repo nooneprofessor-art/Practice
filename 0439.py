870b7d98bb451fbac4de29f7ff976033515648bb
Jones. <jonesmailhub@gmail.com>
2026-01-29 08:54:14 +0500

    Create 0439.py

---
+++ 0439.py
@@ -0,0 +1,10 @@
+jaba = 4
+zhak = 0
+
+print("Знает ли Жаба языков программирование:".upper())
+print(bool(jaba))
+
+print("\nЗнает ли Джонни языков программирование:".upper())
+print(bool(zhak))
+
+print("(Пока не знает)".lower())
