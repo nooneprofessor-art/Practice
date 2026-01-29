54a8738ddd1e09b6c9ada752eeafbad2a183420e
Jones. <jonesmailhub@gmail.com>
2026-01-29 10:02:10 +0500

    Create 0467.py

---
+++ 0467.py
@@ -0,0 +1,9 @@
+else:
+                        print("\nНет дерево на продажу :(")
+                elif action_shop == "домой" or action_shop == "Домой" or action_shop == "Дом" or action_shop == "дом":
+                    loop = False
+                else:
+                    continue 
+
+        else:
+            break
