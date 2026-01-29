047d228645031bf994d44beef67eca1e005428dc
Jones. <jonesmailhub@gmail.com>
2026-01-29 09:01:49 +0500

    Create 0451.py

---
+++ 0451.py
@@ -0,0 +1,13 @@
+num = 5
+counter = 10
+while counter >= 0:
+    print(num)
+    num *= 5
+    counter -= 1
+
+list = ["a", "b", "c", "d"]
+counter2 = 4
+while counter2 > 0:
+    print(list[0])
+    list.pop(0)
+    counter2 -= 1
