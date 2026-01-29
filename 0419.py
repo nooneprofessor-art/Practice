2c2f163da3b57cd96b99f8aacbfef953b595c2d0
Jones. <jonesmailhub@gmail.com>
2026-01-29 09:27:28 +0500

    Create 0419.py

---
+++ 0419.py
@@ -0,0 +1,28 @@
+o = True
+s = False
+
+print(pow(o + o, 4) - s, end="\n\n")
+
+o = int(o)
+s = int(s)
+
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o, end="-")
+print(o)
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s, end="-")
+print(s)
