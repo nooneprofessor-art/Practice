d3a1e5dc38d8dca21ef7fd14b36a9f0fc1ffd085
Jones. <jonesmailhub@gmail.com>
2026-01-29 10:25:47 +0500

    Create 0516.py

---
+++ 0516.py
@@ -0,0 +1,19 @@
+def f1(w1 = "Я ", w2 = "учу ", w3 = "Python", w4 = "!"):
+    words = w1 + w2 + w3 + w4; return words
+print(f1(w3 = "Java"))
+
+def f2(w1 = "Я ", w2 = "учу ", w3 = "Python", w4 = "!"):
+    words = w1 + w2 + w3 + w4; return words
+print(f2(w4 = "?"))
+
+def f3(w1 = "Я ", w2 = "учу ", w3 = "Python", w4 = "!"):
+    words = w1 + w2 + w3 + w4; return words
+print(f3("Ты ", "учишь ", "HTML", "."))
+
+def f4(w1 = "Я ", w2 = "знают ", w3 = "Python ", w4 = "!"):
+    words = w1 + w2 + w3 + w4; return words
+print(f4(w1 = "Они ", w4 = "0_0"))
+
+def f5(w1 = "Она ", w2 = "учу ", w3 = "в ", w4 = "!"):
+    words = w1 + w2 + w3 + w4; return words
+print(f5(w2 = "знаток ", w4 = "программировании."))
