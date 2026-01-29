1ea05475ef44c3a6028139fd0c956833d3f6a734
Jones. <jonesmailhub@gmail.com>
2026-01-29 08:03:45 +0500

    Create 0436.py

---
+++ 0436.py
@@ -0,0 +1,7 @@
+num1 = input("Первое число: ")
+num2 = input("Второе число: ")
+
+transformation_num1 = int(num1)
+transformation_num2 = int(num2)
+
+print(pow(transformation_num1, transformation_num2))
