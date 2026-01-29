696ae17e2e6f3c4b0fc77e196a9eba4b4eccd6c3
Jones. <jonesmailhub@gmail.com>
2026-01-29 10:52:21 +0500

    Create 0590.py

---
+++ 0590.py
@@ -0,0 +1,12 @@
+l = [4, 3, 7, 5, 2, 8]
+k = (7, 3, 8, 9, 1, 4)
+m = {2, 8, 5, 8, 4, 9}
+s = {"num1": 8, "num2": 5, "num3": 3}
+
+def f(i):
+    return min(i)
+    
+print(f(l))
+print(f(k))
+print(f(m))
+print(f(s))
