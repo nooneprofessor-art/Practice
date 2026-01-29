7a58f6987f6d5cbf80f6aeb370e22fb534c318af
Jones. <jonesmailhub@gmail.com>
2026-01-29 11:31:44 +0500

    Create 0660.py

---
+++ 0660.py
@@ -0,0 +1,17 @@
+val = {
+    "resoursec": {
+        "coal": 121,
+        "iron": 50,
+        "gold": 7
+    },
+    "items": {
+        "coin": 100,
+        "exprience": 643837,
+        "skill": "12 lvl"
+    },
+    "info": {
+        "id": 344553,
+        "name": "Sam",
+        "bonus": 555
+    }
+}
